fa7875deb0d0c10dbd33b47de48589af117c2183
Vaishaal Shankar <vaishaal@berkeley.edu>
2018-02-02 20:09:14 +0000

    dumb bug in backward solve

--- backwards_solve.py
+++ backwards_solve.py
@@ -10,9 +10,13 @@ from numpywren.matrix_init import local_numpy_init
 from scipy.linalg import solve
 import argparse
 
-def backwards_solve(bigm, y):
-    x = np.zeros(y.shape)
+def backwards_solve(bigm):
     last_block = bigm._block_idxs(1)[-1]
+    print("Grabbing y")
+    y1 = utils.get_col(bigm, last_block, mmap_loc="/dev/shm/y1")
+    y0 = utils.get_col(bigm, last_block - 1, mmap_loc="/dev/shm/y0")
+    y = np.hstack((y0, y1))[:, -1000:]
+    x = np.zeros(y.shape)
     executor = fs.ProcessPoolExecutor(64)
     for block_idx, (b_start, b_end) in list(zip(bigm._block_idxs(0)[::-1], bigm._blocks(0)[::-1])):
         print("block_idx", block_idx)
@@ -32,22 +36,22 @@ def backwards_solve(bigm, y):
         else:
             rhs = y_block
         x[b_start:b_end] = scipy.linalg.solve_triangular(A_bb, rhs)
-        print("Writing model")
-        np.save("/dev/shm/backsub_{0}".format(bigm.key), x)
+        if (block_idx % 10 == 0):
+            print("Writing model")
+            np.save("/dev/shm/backsub_{0}".format(bigm.key), x)
         compute_end = time.time()
         print("Compute took {0} seconds".format(compute_end - compute_start))
         e = time.time()
         print("Block {0} took {1} seconds".format(block_idx, e - t))
+    np.save("/dev/shm/backsub_{0}".format(bigm.key), x)
     return x
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('matrix_key', type=str, help="LU decomposition")
-    parser.add_argument('label_path', type=str, help="forward substitution")
     args = parser.parse_args()
-    bigm  = BigMatrix(args.matrix_key, bucket="pictureweb")
-    y = np.load(args.label_path)
-    backwards_solve(bigm, y)
+    bigm  = BigMatrix(args.matrix_key, bucket="pictureweb", shape=[1281167, 1282167], shard_sizes=[4096,4096])
+    backwards_solve(bigm)
 
 
 
