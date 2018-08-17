1bd808974ebb2c2220b49a86bf87fa27f5d39525
Vaishaal Shankar <vaishaal@berkeley.edu>
2018-08-17 19:44:47 +0000

    fastqr added

--- numpywren/kernels.py
+++ numpywren/kernels.py
@@ -1,11 +1,36 @@
 import numpy as np
 import scipy.linalg
+import boto3
+import os
 
 def add(*args, **kwargs):
     out = np.zeros(args[0].shape)
     for a in args:
         out += a
     return out
+
+def fast_qr(x):
+    if (not os.path.isfile("/tmp/dgqert3.cpython-36m-x86_64-linux-gnu.so")):
+        with open("/tmp/dgqert3.cpython-36m-x86_64-linux-gnu.so", "wb+") as f:
+            client = boto3.client('s3')
+            bstream = client.get_object(Bucket="numpywrenpublic", Key="shared_sos/dgqert3.cpython-36m-x86_64-linux-gnu.so")["Body"].read()
+            f.write(bstream)
+    import sys
+    sys.path.insert(0, "/tmp/")
+    import dgqert3
+    x = x.copy()
+    t = np.zeros((x.shape[1], x.shape[1]), order='F')
+    dgqert3.dgeqrt3(x.shape[0], x.shape[1], a=x, t=t, info=0)
+    r = np.triu(x)
+    v = np.triu(x.T).T
+    idxs = np.diag_indices(v.shape[0])
+    v[idxs] = 1
+    return v,t,r
+
+
+
+
+
 
 def qr_factor(*blocks, **kwargs):
     ins = np.vstack(blocks)
